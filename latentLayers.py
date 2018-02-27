a2e884f056103962c5922eb0dfe78c4ead258471
Andrea Schioppa <andrea.schioppa@booking.com>
2018-02-27 11:40:50 +0100

    latentCatLayer

--- latentLayers.py
+++ latentLayers.py
@@ -3,26 +3,38 @@ import numpy as np
 import pandas as pd
 
 class latentUnashedCategorical:
+    # static counter
     numInstances = 0
-    def __init__(self,categorical_values, default_index, key_type, num_latent_factors, prior_weights=None):
+
+    def __init__(self,categorical_values,
+                 default_index,
+                 key_type,
+                 num_latent_factors,
+                 prior_weights=None):
         latentUnashedCategorical.numInstances += 1
-        self.lookupDataFrame = pd.DataFrame({"values" : categorical_values,
-                                        "indices" : np.array(xrange(len(categorical_values)))
+        # lookup Table
+        self.lookupDataFrame = pd.DataFrame({"categorical_values" : categorical_values,
+                                        "new_indices" : np.array(xrange(len(categorical_values)))
                                         })
+        # position of default index
         self.default_index = default_index
         self.num_latent_factors = num_latent_factors
 
+        # old values => new indices
         self.lookupTable = tf.contrib.lookup.HashTable(
-                 tf.contrib.lookup.KeyValueTensorInitializer(self.lookupDataFrame.values.values,
-                                                             self.lookupDataFrame.indices.values,
+                 tf.contrib.lookup.KeyValueTensorInitializer(self.lookupDataFrame.categorical_values.values,
+                                                             self.lookupDataFrame.new_indices.values,
                     key_dtype=key_type,value_dtype=tf.int64),
                     default_value = self.default_index)
+        # new indices => old values
         self.inverseLookupTable=tf.contrib.lookup.HashTable(
-                 tf.contrib.lookup.KeyValueTensorInitializer(self.lookupDataFrame.indices.value,
-                                                             self.lookupDataFrame.values.values,
+                 tf.contrib.lookup.KeyValueTensorInitializer(self.lookupDataFrame.new_indices.values,
+                                                             self.lookupDataFrame.categorical_values.values,
 
                     key_dtype=tf.int64,value_dtype=key_type),
                     default_value = categorical_values[self.default_index])
+
+        # can preload weights already trained
         if prior_weights is None:
             self.weights = tf.get_variable(name="latentUnashedCategoricalWeights"+
                                            str(latentUnashedCategorical.numInstances),
---
+++ tests/latenUnashedCategorical.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from aschioppa_tf_layers.latentLayers import latentUnashedCategorical as Lcat
+
+cat1 = ["a", "b", "c"]
+cat2 = [11, 9, 27, -1]
+
+lcat1 = Lcat(cat1, default_index=0, key_type=tf.string, num_latent_factors=4,
+             prior_weights = np.array([[0.1, 0.01, 0.4, 0.5],
+                                         [-0.2,0.02,-0.4,0.5],
+                                         [-0.22,0.5,0.4,0.1]]))
+
+lcat2 = Lcat(cat2, default_index=3, key_type=tf.int64, num_latent_factors=4)
+
+input1 = tf.constant(["a","a","b","b","c","c","e","e"],dtype=tf.string)
+input2 = tf.constant([[11, 9], [27, 27], [-1, -1], [13, 17]],dtype=tf.int64)
+
+look1 = lcat1.getLookupLayer(input1)
+look2 = lcat2.getLookupLayer(input2)
+invlook1 = lcat1.getInverseLookupLayer(look1)
+invlook2 = lcat2.getInverseLookupLayer(look2)
+latentLayer1 = lcat1.getLatentLayer(input1)
+latentLayer1bis = lcat1.getLatentLayer(look1,withLookup=False)
+latentLayer2 = lcat2.getLatentLayer(input2)
+latentLayer2bis = lcat2.getLatentLayer(look2,withLookup=False)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.tables_initializer())
+    print sess.run([look1])
+    print sess.run([invlook1])
+    print sess.run([latentLayer1])
+    print sess.run([latentLayer1bis])
+    print sess.run([look2])
+    print sess.run([invlook2])
+    print sess.run([latentLayer2])
+    print sess.run([latentLayer2bis])
+
+
